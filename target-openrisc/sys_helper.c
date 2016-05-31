46e88739046aa8866bb786575fa5431841d95556
Dalmon Ian Martins de Oliveira <dalmonianm@gmail.com>
2016-05-31 10:33:27 -0300

    Adds l.mfspr support to AESR
    Signed-off-by: Dalmon Ian Martins de Oliveira <dalmonianm@gmail.com>

--- target-openrisc/sys_helper.c
+++ target-openrisc/sys_helper.c
@@ -200,6 +200,9 @@ target_ulong HELPER(mfspr)(CPUOpenRISCState *env,
     case TO_SPR(0, 4): /* IMMUCFGR */
         return env->immucfgr;
 
+    case TO_SPR(0, 13): /* AESR */
+        return env->aesr;
+
     case TO_SPR(0, 16): /* NPC */
         return env->npc;
 
