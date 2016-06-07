8bf292f83cf221ef670d3c0ec14d3143c031429e
Dalmon Ian Martins de Oliveira <dalmonianm@gmail.com>
2016-06-07 13:17:15 -0300

    Changes mult32 helper function
    The mul32 function has changed to fit the new exception scheme.
    
    Signed-off-by: Dalmon Ian Martins de Oliveira <dalmonianm@gmail.com>

--- target-openrisc/int_helper.c
+++ target-openrisc/int_helper.c
@@ -46,37 +46,25 @@ target_ulong HELPER(fl1)(target_ulong x)
 uint32_t HELPER(mul32)(CPUOpenRISCState *env,
                        uint32_t ra, uint32_t rb)
 {
-    uint64_t result;
-    uint32_t high, cy;
+    uint32_t res;
+    uint32_t aeon; /* Arithmetic exception on */
 
     OpenRISCCPU *cpu = openrisc_env_get_cpu(env);
 
-    result = (uint64_t)ra * rb;
-    /* regisiers in or32 is 32bit, so 32 is NOT a magic number.
-       or64 is not handled in this function, and not implement yet,
-       TARGET_LONG_BITS for or64 is 64, it will break this function,
-       so, we didn't use TARGET_LONG_BITS here.  */
-    high = result >> 32;
-    cy = result >> (32 - 1);
+    aeon = (env->cpucfgr & CPUCFGR_AECSRP) && (env->sr & SR_OVE);
 
-    if ((cy & 0x1) == 0x0) {
-        if (high == 0x0) {
-            return result;
+    res = ra * rb;
+
+    /* Sets overflow flag */
+    if (((~ra & ~rb & res) >> 31 | (ra & rb & ~res) >> 31) != 0) {
+        env->sr |= SR_OV;
+        if (aeon && (env->aecr & AECR_OVMULE) == AECR_OVMULE) {
+            env->aesr |= AESR_OVMULE;
+			raise_exception(cpu, EXCP_RANGE);
         }
     }
 
-    if ((cy & 0x1) == 0x1) {
-        if (high == 0xffffffff) {
-            return result;
-        }
-    }
-
-    cpu->env.sr |= (SR_OV | SR_CY);
-    if (cpu->env.sr & SR_OVE) {
-        raise_exception(cpu, EXCP_RANGE);
-    }
-
-    return result;
+    return res;
 }
 
 uint32_t HELPER(adder)(CPUOpenRISCState *env,
